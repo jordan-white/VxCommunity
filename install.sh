a7316264ead697e0983e77f4a69b2f2631df6ea4
Jan Miller <jan.miller@payload-security.com>
2017-01-06 13:09:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -266,14 +266,14 @@ checks() {
         exit 1
     fi
 
-    # Make sure the system uptime is atleast 10 minutes and the dpkg has finished it's processes
+    # Make sure the system uptime is atleast 5 minutes and the dpkg has finished it's processes
     systemUptime=$(uptime -p | awk {'print $2'} | xargs)
 
-    if [ "$systemUptime" -lt 10 ]; then
+    if [ "$systemUptime" -lt 5 ]; then
 
         failure
         echo "Fatal error: the system uptime is only: $systemUptime minutes"
-        echo "Because dpkg might be locked just after system boot, then it is advised to wait 10 minutes until installing VxStream. Exiting for now..."
+        echo "Because dpkg might be locked just after system boot, it is advised to wait 5 minutes before installing VxStream. Exiting for now..."
         exit 1
 
     fi
