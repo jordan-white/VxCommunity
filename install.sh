92cb8671f12fcf8acd295e768059d1f4a22af1ee
millerjkd <jan.miller@payload-security.com>
2017-07-26 10:43:09 +0200

    - cosmetic

--- install.sh
+++ install.sh
@@ -596,6 +596,7 @@ main() {
     else
         cd "$installDir"/VxBootstrapUI/scripts && sudo installUser="$installUser" installUserPassword="$installUserPassword" termColumns="$termColumns" ./init.sh
     fi
+
     if [ $? -eq 0 ]; then
 
         exit 0
@@ -605,7 +606,6 @@ main() {
         exit 1
 
     fi
-
 }
 
 # Success and error message colouring
